f2197270e7895fe7d8e30dcb2bff5348a8b730f0
한예지 <146055499+hyez2@users.noreply.github.com>
2024-07-20 16:24:16 +0900

    Update merge.py

--- merge.py
+++ merge.py
@@ -1,6 +1,5 @@
 def greet():
-    print("Hello from the jiyxxnseo branch")
-    print("Hello from the main branch")
+    print("Hello from the hyez branch")
 
 def add(a, b):
     return a + b
@@ -13,17 +12,17 @@ def multiply(a, b):
 
 def divide(a, b):
     if b == 0:
-        return "Cannot divide by zero" 
+        return "Cannot divide by zero"
     return a / b
 
 def main():
-    print("This is the main function") 
-    greet() 
-    result_add = add(10, 5) 
-    result_sub = subtract(10, 5) 
-    result_mul = multiply(10, 5) 
-    result_div = divide(10, 5) 
-    print(f"Add: {result_add}, Subtract: {result_sub}, Multiply")
+    print("This is the main function")
+    greet()
+    result_add = add(10, 5)
+    result_sub = subtract(10, 5)
+    result_mul = multiply(10, 5)
+    result_div = divide(10, 5)
+    print(f"Add: {result_add}, Subtract: {result_sub}, Multiply: {result_mul}, Divide: {result_div}")
 
 if __name__ == "__main__":
     main()
